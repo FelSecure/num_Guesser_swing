868e231295d389e063924a384530b1a0bb96ed29
Felix Schumacher <felixschumacher@ymail.com>
2022-03-08 19:12:42 +0100

    first commit

---
+++ src/gui.java
@@ -0,0 +1,47 @@
+//Usually you will require both swing and awt packages
+// even if you are working with just swings.
+import javax.swing.*;
+import java.awt.*;
+
+
+class GUI {
+
+    public static void main(String args[]) {
+
+
+        //Input Dialog
+        JOptionPane.showInputDialog("Obere Nummern Grenze:");
+        JOptionPane.showInputDialog("Untere Nummern Grenze:");
+
+
+        //Creating the Frame
+        JFrame frame = new JFrame("Number Guesser");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("Help");
+        mb.add(m1);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Number:");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+    }
+}
